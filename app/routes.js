5a250cc536389f45823c50b4bc43664a68749d5d
jo lumley <jo.lumley1@nhs.net>
2024-10-04 09:48:08 +0100

    add availability and year overview

--- app/routes.js
+++ app/routes.js
@@ -5,4 +5,16 @@ const router = express.Router();
 
 // Add your routes here - above the module.exports line
 
+// v1 create availability routes //
+
+router.get(/addSessionType/, function (req, res) {
+    if (req.query.radioGroup === "Repeat" ) {
+        res.redirect('select-days');
+    }
+    else {
+        res.redirect('session-date');
+    }
+});
+
 module.exports = router;
+
