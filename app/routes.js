70c2b741a58d998778c9e2b6925c38c9cd1de279
jo lumley <jo.lumley1@nhs.net>
2024-10-07 16:02:13 +0100

    ur tweaks and view availability

--- app/routes.js
+++ app/routes.js
@@ -9,10 +9,19 @@ const router = express.Router();
 
 router.get(/addSessionType/, function (req, res) {
     if (req.query.radioGroup === "Repeat" ) {
-        res.redirect('select-days');
+        res.redirect('availability-date-range');
     }
     else {
         res.redirect('session-date');
+    }
+});
+
+router.get(/siteOpen/, function (req, res) {
+    if (req.query.radioGroup === "yes" ) {
+        res.redirect('session-time-capacity2');
+    }
+    else {
+        res.redirect('check-days4');
     }
 });
 
